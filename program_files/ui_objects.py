4bf389b6dfb6332acead7e85a7f33412901c8c43
anna <anna8g5@mail.ru>
2021-11-27 18:55:51 +0300

    add worked sliders

--- program_files/ui_objects.py
+++ program_files/ui_objects.py
@@ -4,7 +4,7 @@ pygame.font.init()
 
 
 class TextBox:
-    #class textboxes
+    # class textboxes
     def __init__(self, position, bg_image, font, font_color, text, aligment):
         '''
         init for class Textbox
@@ -24,7 +24,7 @@ class TextBox:
         self.aligment = aligment
         f = pygame.font.Font(None, self.font)
         text_surface = f.render(self.text, True, self.font_color)
-        #count size based on size of text:
+        # count size based on size of text:
         self.size = (text_surface.get_width() * 6 // 5, text_surface.get_height() * 6 // 5)
         if self.aligment == 'center':
             self.position = [self.position[0] - self.size[0] / 2, self.position[1] - self.size[1] / 2]
@@ -36,8 +36,8 @@ class TextBox:
         rect_surface = pygame.image.load(self.bg_image).convert_alpha()
         rect_surface = pygame.transform.scale(rect_surface, self.size)
         surface.blit(rect_surface, (0, 0))
-        
-        #write text
+
+        # write text
         f = pygame.font.Font(None, self.font)
         text_surface = f.render(self.text, True, self.font_color)
         x_pos = self.size[0] / 2
@@ -73,11 +73,11 @@ class Button(TextBox):
         if event.type == pygame.MOUSEBUTTONDOWN:
             rho = [event.pos[0] - self.position[0], event.pos[1] - self.position[1]]
             if rho[0] <= self.size[0] and rho[0] >= 0 and rho[1] <= self.size[1] and rho[1] >= 0:
-                self.func(self)
+                self.func()
 
 
 class Slider():
-    def __init__(self, position, image, on_color, off_color, size, value):
+    def __init__(self, position, image, on_color, off_color, size, system, name, value=0.5):
         '''
         init for class Slider (Polzunok)
         position - coords of left high corner
@@ -94,6 +94,8 @@ class Slider():
         self.size = size
         self.value = value
         self.condition = False
+        self.system = system
+        self.name = name
         self.circle_scale = 1.3
         self.circle_size = (int(self.size[1] * self.circle_scale), int(self.size[1] * self.circle_scale))
         self.circle_pos = (self.position[0] + self.size[0] * self.value, self.position[1] + self.circle_size[1] / 2)
@@ -105,78 +107,139 @@ class Slider():
         return self.value
 
     def get_surface(self):
+        # get surface of drawing textbox
+        # draw background image:        
         surface = pygame.Surface((self.size[0] * self.circle_scale, self.size[1] * self.circle_scale), pygame.SRCALPHA, 32)
-        pygame.draw.rect(surface, self.on_color, ((0,self.size[1]*(self.circle_scale-1)/2), (self.size[0]*self.value, self.size[1])))
-        pygame.draw.rect(surface, self.off_color, ((self.size[0]*self.value,self.size[1]*(self.circle_scale-1)/2), (self.size[0]*(1-self.value), self.size[1])))
+        #draw rects:
+        pygame.draw.rect(surface, self.on_color, ((0, self.size[1] * (self.circle_scale - 1) / 2), (self.size[0] * self.value, self.size[1])))
+        pygame.draw.rect(surface, self.off_color, ((self.size[0] * self.value, self.size[1] * (self.circle_scale - 1) / 2), (self.size[0] * (1 - self.value), self.size[1])))
+        #draw circle:
         circle_surface = pygame.image.load(self.image).convert_alpha()
         circle_surface = pygame.transform.scale(circle_surface, self.circle_size)
+        circle_surface.set_colorkey((253, 253, 253))
         surface.blit(circle_surface, (self.size[0] * self.value - self.circle_size[0] / 2, 0))
-        
         return surface
 
     def click(self, event):
+        '''
+        reaction for events
+        event - pygame Event
+        '''
         if event.type == pygame.MOUSEBUTTONDOWN:
+            #change status of object if button clicked
             rho = (event.pos[0] - self.circle_pos[0], event.pos[1] - self.circle_pos[1])
             if rho[0] ** 2 + rho[1] ** 2 <= (self.circle_size[1] / 2) ** 2:
                 self.condition = True
+                
         if event.type == pygame.MOUSEMOTION and self.condition:
-            self.value = (event.pos[0]-self.position[0])/(self.size[0])
-            
-def settings(obj):
-    print('settings')
-    obj.bg_image = 'pin.jpg'
-    global objects
-    menu = TextBox((screen.get_width() / 2, screen.get_height() * 2 // 8), 'box.jpg', 50, 'black', 'Settings menu', 'center')
-    volume = TextBox((screen.get_width() / 6, screen.get_height() * 3 // 8), 'box.jpg', 50, 'black', 'Volume', 'left')
-    bg_dim = TextBox((screen.get_width() / 6, screen.get_height() * 4 // 8), 'box.jpg', 50, 'black', 'Dim', 'left')
-    bg_blur = TextBox((screen.get_width() / 6, screen.get_height() * 5 // 8), 'box.jpg', 50, 'black', 'Blur', 'left')
-    offset = TextBox((screen.get_width() / 6, screen.get_height() * 6 // 8), 'box.jpg', 50, 'black', 'Offset', 'left')
-    volume_slider = Slider((screen.get_width() * 4 // 6, screen.get_height() * 3 // 8), 'circle.png', 'yellow', 'black', (screen.get_width() / 5, screen.get_height() / 60), 0.6)
-    objects = [menu, volume, bg_dim, bg_blur, offset, volume_slider]
+            #change position of circle and value 
+            if event.pos[0] - self.position[0] <= self.size[0] - self.circle_size[0] / 2 and event.pos[0] - self.position[0] >= self.circle_size[0] / 2:
+                self.value = (event.pos[0] - self.position[0] - self.circle_size[0] / 2) / (self.size[0] - self.circle_size[0])
+                self.circle_pos = (self.position[0] + self.size[0] * self.value, self.position[1] + self.circle_size[1] / 2)
+                system.set(self.name, self.value)
+                
+        if event.type == pygame.MOUSEBUTTONUP and self.condition:
+            #change condition of object if clicked
+            self.condition = False
+
+
+class System():
+    #class that have all parametries of during menu-pack or game???
+    def __init__(self, bg_image, volume=0.5, dim=0.5, blur=0.5, offset=0.5):
+        '''
+        init for class System
+        bg_image - image of background screen
+        volume - volume of sound
+        dim - characterist of sound
+        blur - characterist of sound
+        offset - contacting sound and picture
+        '''
+        self.volume = volume
+        self.dim = dim
+        self.blur = blur
+        self.offset = offset
+        self.screen =''
+        self.bg_image=bg_image
+        self.objects = []
+
+    def set(self, name, value):
+        #set some parametres of game
+        if name == 'volume':
+            self.volume = value
+        if name == 'blur':
+            self.blur = value
+        if name == 'dim':
+            self.dim = value
+        if name == 'offset':
+            self.offset = value
+
+    def menu(self):
+        #open menu
+        screen = self.screen
+        menu = TextBox((screen.get_width() / 2, screen.get_height() * 2 // 7), 'box.jpg', 50, 'black', 'Menu', 'center')
+        setting = Button((screen.get_width() / 2, screen.get_height() * 3 // 7), 'rect.png', 50, 'blue', 'Settings', 'center', self.settings)
+        start = Button((screen.get_width() / 2, screen.get_height() * 4 // 7), 'rect.png', 50, 'blue', 'Start', 'center', fun2)
+        exit = Button((screen.get_width() / 2, screen.get_height() * 5 // 7), 'rect.png', 50, 'blue', 'Exit', 'center', fun3)
+        self.objects = [menu, setting, start, exit]
+
+    def settings(self):
+        #open settings-menu
+        screen = self.screen
+        menu_box = TextBox((screen.get_width() / 2, screen.get_height() * 2 // 8), 'box.jpg', 50, 'black', 'Settings menu', 'center')
+        volume = TextBox((screen.get_width() / 6, screen.get_height() * 3 // 8), 'box.jpg', 50, 'black', 'Volume', 'left')
+        bg_dim = TextBox((screen.get_width() / 6, screen.get_height() * 4 // 8), 'box.jpg', 50, 'black', 'Dim', 'left')
+        bg_blur = TextBox((screen.get_width() / 6, screen.get_height() * 5 // 8), 'box.jpg', 50, 'black', 'Blur', 'left')
+        offset = TextBox((screen.get_width() / 6, screen.get_height() * 6 // 8), 'box.jpg', 50, 'black', 'Offset', 'left')
+        volume_slider = Slider((screen.get_width() * 4 // 6, screen.get_height() * 3 // 8), 'circle.png', 'yellow', 'black', (screen.get_width() / 5, screen.get_height() / 60), self, 'volume', self.volume)
+        dim_slider = Slider((screen.get_width() * 4 // 6, screen.get_height() * 4 // 8), 'circle.png', 'green', 'blue', (screen.get_width() / 5, screen.get_height() / 60), self, 'dim', self.dim)
+        blur_slider = Slider((screen.get_width() * 4 // 6, screen.get_height() * 5 // 8), 'circle.png', (108, 225, 64), (142, 0, 0), (screen.get_width() / 5, screen.get_height() / 60), self, 'blur', self.blur)
+        offset_slider = Slider((screen.get_width() * 4 // 6, screen.get_height() * 6 // 8), 'circle.png', (228, 228, 61), (96, 49, 74), (screen.get_width() / 5, screen.get_height() / 60), self, 'offset', self.offset)
+        exit = Button((screen.get_width() * 4 // 6, screen.get_height() * 7 // 8), 'rect.png', 50, 'blue', 'Back', 'left', self.menu)
+        self.objects = [menu_box, volume, bg_dim, bg_blur, offset, volume_slider, dim_slider, blur_slider, offset_slider, exit]
+        
+    def play(self):
+        #start menu-window
+        FPS = 30
+        self.screen = pygame.display.set_mode((600, 800))
+        bg_surface = pygame.image.load(self.bg_image).convert_alpha()
+        bg_surface = pygame.transform.scale(bg_surface, (self.screen.get_width(), self.screen.get_height()))
+        self.screen.blit(bg_surface, (0, 0))
+        pygame.display.update()
+        clock = pygame.time.Clock()
+        finished = False
+        
+        system.menu()
+        
+        while not finished:
+            clock.tick(FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    finished = True
+                for obj in system.objects:
+                    if type(obj) != TextBox:
+                        obj.click(event)
+        
+            system.screen.blit(bg_surface, (0, 0))
+            for obj in system.objects:
+                system.screen.blit(obj.get_surface(), obj.position)
+        
+            pygame.display.update()        
 
 
 def fun2(obj):
+    #example function
     print('start')
     obj.bg_image = 'pin.jpg'
 
 
 def fun3(obj):
+    #example function
     print('exit')
     obj.bg_image = 'pin.jpg'
 
 
-# make background
-FPS = 30
-screen = pygame.display.set_mode((600, 800))
-bg_surface = pygame.image.load('bg.jpg').convert_alpha()
-bg_surface = pygame.transform.scale(bg_surface, (screen.get_width(), screen.get_height()))
-screen.blit(bg_surface, (0, 0))
-pygame.display.update()
-clock = pygame.time.Clock()
-finished = False
-
-# example and check working
-
-
-menu = TextBox((screen.get_width() / 2, screen.get_height() * 2 // 7), 'box.jpg', 50, 'black', 'Menu', 'center')
-settings = Button((screen.get_width() / 2, screen.get_height() * 3 // 7), 'rect.png', 50, 'blue', 'Settings', 'center', settings)
-start = Button((screen.get_width() / 2, screen.get_height() * 4 // 7), 'rect.png', 50, 'blue', 'Start', 'center', fun2)
-exit = Button((screen.get_width() / 2, screen.get_height() * 5 // 7), 'rect.png', 50, 'blue', 'Exit', 'center', fun3)
-objects = [menu, settings, start, exit]
-
-while not finished:
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            finished = True
-        for obj in objects:
-            if type(obj) != TextBox:
-                obj.click(event)
-
-    screen.blit(bg_surface, (0, 0))
-    for obj in objects:
-        screen.blit(obj.get_surface(), obj.position)
-
-    pygame.display.update()
+# make system
+system = System('bg.jpg')
+system.play()
 
 pygame.quit()
